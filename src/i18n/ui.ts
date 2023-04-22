81b3ea962de042fa1044d1e8efec1272c65fec4a
Lex Nousagi <alexmiyin@gmail.com>
2023-04-22 12:37:40 +0300

    feat(*): initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+  en: 'English',
+  ua: 'Українська'
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'nav.title': 'Babyn Yar',
+    'nav.visit': 'Visit',
+    'nav.education': 'Education',
+    'nav.connect': 'Connect',
+    'nav.support': 'Support',
+    'footer.main': 'National Historical and Memorial Reserve "Babyn Yar"',
+    'footer.address': 'Ukraine, 04119  Kyiv, st. Yuriy  Ilyenka, 44'
+  },
+  ua: {
+    'nav.title': 'Бабин Яр',
+    'nav.visit': 'Відвідати',
+    'nav.education': 'Освіта',
+    'nav.connect': "Зв'язок",
+    'nav.support': 'Підтримати',
+    'footer.main': 'Національний історико-меморіальний заповідник "Бабин Яр"',
+    'footer.address': 'Україна, 04119  м. Київ, вул. Юрія  Іллєнка, 44'
+  }
+} as const
