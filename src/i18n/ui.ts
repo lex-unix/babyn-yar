c38351ff27b63a4880bc93d610107cefd72d83bf
Lex Nousagi <alexmiyin@gmail.com>
2023-04-30 22:32:42 +0300

    feat(i18n): add translations for contact form, improve navbar translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,19 +9,41 @@ export const ui = {
   en: {
     'nav.title': 'Babyn Yar',
     'nav.visit': 'Visit',
+    'nav.visit.events': 'Events',
     'nav.education': 'Education',
+    'nav.education.history': 'Historiy',
+    'nav.education.map': 'Map of Holocaust',
+    'nav.education.archive': 'Archive',
     'nav.connect': 'Connect',
+    'nav.connect.find-us': 'Find us',
     'nav.support': 'Support',
+    'nav.support.partnership': 'Partnership',
     'footer.main': 'National Historical and Memorial Reserve "Babyn Yar"',
-    'footer.address': 'Ukraine, 04119  Kyiv, st. Yuriy  Ilyenka, 44'
+    'footer.address': 'Ukraine, 04119  Kyiv, st. Yuriy  Ilyenka, 44',
+    'contact.form.surname': 'Surname',
+    'contact.form.name': 'Name',
+    'contact.form.email': 'Email',
+    'contact.form.message': 'Message text',
+    'contact.form.button': 'Send'
   },
   ua: {
     'nav.title': 'Бабин Яр',
     'nav.visit': 'Відвідати',
+    'nav.visit.events': 'Події',
     'nav.education': 'Освіта',
+    'nav.education.history': 'Історія',
+    'nav.education.map': 'Мапа Голокосту',
+    'nav.education.archive': 'Архів',
     'nav.connect': "Зв'язок",
+    'nav.connect.find-us': 'Знайти нас',
     'nav.support': 'Підтримати',
+    'nav.support.partnership': 'Партнерство',
     'footer.main': 'Національний історико-меморіальний заповідник "Бабин Яр"',
-    'footer.address': 'Україна, 04119  м. Київ, вул. Юрія  Іллєнка, 44'
+    'footer.address': 'Україна, 04119  м. Київ, вул. Юрія  Іллєнка, 44',
+    'contact.form.surname': 'Призвище',
+    'contact.form.name': "Ім'я",
+    'contact.form.email': 'Email',
+    'contact.form.message': 'Текст повідомлення',
+    'contact.form.button': 'Надіслати'
   }
 } as const
