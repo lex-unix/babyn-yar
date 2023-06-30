573d9d6559b11b27d362af50e2ba5d8baeb37721
Lex Nousagi <alexmiyin@gmail.com>
2023-06-30 12:28:54 +0300

    fix(astro): incorrect site url leads to incorrect canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.babynyar.org.ua',
+  site: 'https://babynyar.org.ua',
   integrations: [
     tailwind(),
     react(),
