73bf6e80bf5107f22fab890a18004f7cdf56b1d1
Lex Nousagi <alexmiyin@gmail.com>
2023-07-28 12:11:23 +0300

    chore(seo): add all site pages into the DOM

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
 export const EMAIL = 'babinyar@ukr.net'
+
+export const PAGES = [
+  'visit/events',
+  'education/history',
+  'education/map',
+  'education/archive',
+  'connect/find-us',
+  'support/partnership'
+]
