9f2599484b4911dbf432ff6b6c8ff8507f08ff53
Lex Nousagi <alexmiyin@gmail.com>
2023-08-03 15:47:04 +0300

    chore(ts): remove `expect-error`

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -8,7 +8,6 @@ export function getLangFromUrl(url: URL) {
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    // @ts-expect-error
     return ui[lang][key] || ui[defaultLang][key]
   }
 }
