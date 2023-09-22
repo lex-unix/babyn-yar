3f8d0a8ae87ff3f6a47a7b3e0c4abaa2820c1bea
Lex Miin <alexmiyin@gmail.com>
2023-09-22 16:14:29 +0300

    feat(shared): add utility function to work with searchParams

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -6,11 +6,33 @@ export const stripLangFromSlug = (slug: string) =>
 
 export const getLangFromSlug = (slug: string) => slug.split('/')[0]
 
-/*
-  helper function to get types from `Object.keys()`
-*/
+// helper function to get types from `Object.keys()`
 export const objectKeys = <T extends Record<string, any>>(
   obj: T
 ): (keyof T)[] => {
   return Object.keys(obj) as (keyof T)[]
+}
+
+// merge existing searchParams with new ones
+export function addSearchParams(url: URL, params: Record<string, string> = {}) {
+  let searchParams = url.searchParams
+
+  for (const [key, value] of Object.entries(params)) {
+    searchParams.set(key, value)
+  }
+
+  let newUrl = new URL(`${url.origin}${url.pathname}`)
+  newUrl.search = searchParams.toString()
+  return newUrl
+}
+
+// get searchParams where keys will be typed
+export function getParams<K extends string>(
+  url: URL,
+  ...keys: K[]
+): Record<K, string | null> {
+  return keys.reduce((params, key) => {
+    params[key] = url.searchParams.get(key)
+    return params
+  }, {} as Record<K, string | null>)
 }
