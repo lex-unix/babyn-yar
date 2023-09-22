8757b2b9706cb50083360fd394c23f758cd9d1ab
Lex Miin <alexmiyin@gmail.com>
2023-09-22 17:13:59 +0300

    docs(shared): add proper functions documentation

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -13,8 +13,17 @@ export const objectKeys = <T extends Record<string, any>>(
   return Object.keys(obj) as (keyof T)[]
 }
 
-// merge existing searchParams with new ones
-export function addSearchParams(url: URL, params: Record<string, string> = {}) {
+/**
+ * Adds search parameters to a given URL.
+ *
+ * @param {URL} url - The URL to add the search parameters to.
+ * @param {Object.<string, string>} [params={}] - An object containing the search parameters to add.
+ * @returns {URL} - The URL with the added search parameters.
+ */
+export function addSearchParams(
+  url: URL,
+  params: Record<string, string> = {}
+): URL {
   let searchParams = url.searchParams
 
   for (const [key, value] of Object.entries(params)) {
@@ -26,7 +35,14 @@ export function addSearchParams(url: URL, params: Record<string, string> = {}) {
   return newUrl
 }
 
-// get searchParams where keys will be typed
+/**
+ * Gets the parameters from a given URL.
+ *
+ * @param {URL} url - The URL to get the parameters from.
+ * @param {string[]} keys - The keys of the parameters to get.
+ *
+ * @returns {Record<K, string | null>} A record of the parameters with the given keys.
+ */
 export function getParams<K extends string>(
   url: URL,
   ...keys: K[]
