912a0d846be1035c044a383a4e03fb9da1617fe0
Lex Miin <alexmiyin@gmail.com>
2023-09-24 18:24:46 +0300

    feat(admin): add custom input compnent

---
+++ apps/admin/src/lib/components/Input.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  export let type: string = 'text'
+  export let name: string
+  export let label: string
+  export let required: boolean = false
+
+  const dispatch = createEventDispatcher()
+
+  function handleChange(
+    e: Event & { currentTarget: EventTarget & HTMLInputElement }
+  ) {
+    dispatch('change', {
+      name,
+      value: e.currentTarget.value
+    })
+  }
+</script>
+
+<label class="block text-gray-400">
+  {label}
+  <input
+    {type}
+    {name}
+    {required}
+    on:change={handleChange}
+    class="mt-1.5 block h-10 w-full rounded border px-2 text-base text-gray-900 outline-none hover:border-sky-400 focus:border-sky-400 focus:ring focus:ring-sky-100"
+  />
+</label>
--- apps/admin/src/lib/components/index.ts
+++ apps/admin/src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Sidebar } from './Sidebar.svelte'
 export { default as SidebarLink } from './SidebarLink.svelte'
 export { default as SidebarLinkLabel } from './SidebarLinkLabel.svelte'
+export { default as Input } from './Input.svelte'
