833bc3d00a713aa2c29161ac98402c14a27f970e
Lex Miin <alexmiyin@gmail.com>
2023-10-14 14:32:56 +0300

    refactor(admin): preparation for login page
    login page shouldn't have sidebar, but all other pages should;
    by grouping every page into the (app) group we keep the same layout for
    the group, but break out from the layout for login page

---
+++ apps/admin/src/routes/(app)/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Sidebar, Toaster, SidebarLink, SidebarLinkLabel } from '$components'
+  import { Layers, Image, Cog } from 'lucide-svelte'
+</script>
+
+<div class="flex h-screen bg-gray-50 text-gray-900">
+  <Sidebar>
+    <SidebarLink href="/content">
+      <Layers size={20} class="min-w-[20px]" />
+      <SidebarLinkLabel>Контент</SidebarLinkLabel>
+    </SidebarLink>
+    <SidebarLink href="/assets">
+      <Image size={20} class="min-w-[20px]" />
+      <SidebarLinkLabel>Медіа файли</SidebarLinkLabel>
+    </SidebarLink>
+    <SidebarLink href="/settings">
+      <Cog size={20} class="min-w-[20px]" />
+      <SidebarLinkLabel>Налаштування</SidebarLinkLabel>
+    </SidebarLink>
+  </Sidebar>
+  <div class="flex-1 overflow-y-auto overflow-x-hidden px-5 py-10">
+    <slot />
+  </div>
+</div>
+
+<Toaster />
