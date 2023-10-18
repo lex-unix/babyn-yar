7494f4697cbef4ecab0dd0c500b027e80b1bda6b
Lex Miin <alexmiyin@gmail.com>
2023-10-18 15:21:18 +0300

    feat(tip-tap-utils): add helper for converting json to html using tip-tap

---
+++ packages/tip-tap-utils/index.ts
@@ -0,0 +1,22 @@
+import Link from '@tiptap/extension-link'
+import TextAlign from '@tiptap/extension-text-align'
+import Underline from '@tiptap/extension-underline'
+import StarterKit from '@tiptap/starter-kit'
+import Image from '@tiptap/extension-image'
+import { Video } from './video'
+import { generateHTML as tipTapGenerateHTML } from '@tiptap/html'
+
+export const extensions = [
+  StarterKit,
+  Link,
+  TextAlign.configure({
+    types: ['heading', 'paragraph']
+  }),
+  Image,
+  Underline,
+  Video
+]
+
+export function generateHTML(json: any) {
+  return tipTapGenerateHTML(json, extensions)
+}
