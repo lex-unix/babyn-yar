dc1c0d997bb6391bacad22761ee43aebbb6b6cf9
Lex Miin <alexmiyin@gmail.com>
2023-10-21 12:03:48 +0300

    refactor(admin): use event handler instead on binding to allow dynamic input type

--- apps/admin/src/lib/components/Input.svelte
+++ apps/admin/src/lib/components/Input.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+  import type { HTMLInputTypeAttribute } from 'svelte/elements'
+
   export let value: string = ''
   export let name: string
   export let label: string
   export let required: boolean = false
   export let error: string | undefined = undefined
+  export let type: HTMLInputTypeAttribute = 'text'
+
+  function change(
+    e: Event & {
+      currentTarget: EventTarget & HTMLInputElement
+    }
+  ) {
+    value = e.currentTarget.value
+  }
 </script>
 
 <label class="block text-gray-400">
@@ -14,8 +25,9 @@
   <input
     {name}
     {required}
-    type="text"
-    bind:value
+    {type}
+    {value}
+    on:input={change}
     class="mt-1.5 block h-10 w-full rounded border px-2 text-base text-gray-900 outline-none hover:border-teal-400 focus:border-teal-400 focus:ring focus:ring-teal-100"
   />
 </label>
