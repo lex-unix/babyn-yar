a6cbcd718b689a74a6b9e278f8c46c4a6885c9f2
Lex Miin <alexmiyin@gmail.com>
2023-10-31 10:53:05 +0200

    refactor(admin): redirect to content page from index page

--- apps/admin/src/routes/(app)/+page.svelte
+++ apps/admin/src/routes/(app)/+page.svelte
@@ -1,11 +1,4 @@
 <script>
-  import { user } from '$lib/stores'
+  import { goto } from '$app/navigation'
+  goto('/content')
 </script>
-
-<div class="h-full p-10">
-  <h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
-  <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-  </p>
-  <h1>{JSON.stringify($user)}</h1>
-</div>
