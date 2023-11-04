37628bfbd7f34495c1a9d61ed5b1758362864e5e
Lex Miin <alexmiyin@gmail.com>
2023-11-04 11:10:30 +0200

    feat(db): add holocaust_documents table

---
+++ migrations/000012_add_holocaust_documents_table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS holocaust_documents;
---
+++ migrations/000012_add_holocaust_documents_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS holocaust_documents (
+  id bigserial PRIMARY KEY,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  title text NOT NULL,
+  description text NOT NULL,
+  content text NOT NULL,
+  lang text NOT NULL,
+  cover text NOT NULL,
+  version integer NOT NULL DEFAULT 1,
+  user_id bigint NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
