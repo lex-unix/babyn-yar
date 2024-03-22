4219c9e6998e1f50df24dbc204e8daf50fcef7ca
Lex Miin <alexmiyin@gmail.com>
2024-03-22 14:24:06 +0200

    fix: disable ESLint rule for unused vars in EditorSkeleton
    -  Add an ESLint disable comment for the no-unused-vars rule in the EditorSkeleton.svelte component to prevent linting errors for intentional unused variables in a loop.

--- apps/admin/src/lib/components/Skeletons/EditorSkeleton.svelte
+++ apps/admin/src/lib/components/Skeletons/EditorSkeleton.svelte
@@ -1,4 +1,5 @@
 <div class="space-y-5">
+  <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
   {#each Array(8) as _, i (i)}
     <div class="space-y-2">
       <div class="h-5 w-full animate-pulse rounded-md bg-gray-200" />
