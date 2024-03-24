9dd4a861d7accf6653b420d2d6cc33aaded87942
Lex Miin <alexmiyin@gmail.com>
2024-03-24 14:54:04 +0200

    feat(api): upadte handlers for events to include `OccuredOn` property

--- cmd/api/events.go
+++ cmd/api/events.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/lex-unix/babyn-yar/internal/data"
 	"github.com/lex-unix/babyn-yar/internal/validator"
@@ -11,11 +12,12 @@ import (
 
 func (app *application) createEventHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title       string `json:"title"`
-		Description string `json:"description"`
-		Content     string `json:"content"`
-		Lang        string `json:"lang"`
-		Cover       string `json:"cover"`
+		Title       string    `json:"title"`
+		Description string    `json:"description"`
+		Content     string    `json:"content"`
+		Lang        string    `json:"lang"`
+		Cover       string    `json:"cover"`
+		OccuredOn   time.Time `json:"occuredOn"`
 	}
 
 	err := app.readJson(w, r, &input)
@@ -32,6 +34,7 @@ func (app *application) createEventHandler(w http.ResponseWriter, r *http.Reques
 		Content:     input.Content,
 		Lang:        input.Lang,
 		Cover:       input.Cover,
+		OccuredOn:   input.OccuredOn,
 		UserID:      user.ID,
 	}
 
@@ -73,7 +76,7 @@ func (app *application) listEventsHandler(w http.ResponseWriter, r *http.Request
 	input.Filters.PageSize = app.readInt(qs, "page_size", 10, v)
 
 	input.Filters.Sort = app.readString(qs, "sort", "-created_at")
-	input.Filters.SortSafelist = []string{"created_at", "-created_at"}
+	input.Filters.SortSafelist = []string{"created_at", "-created_at", "occured_on", "-occured_on"}
 
 	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
@@ -164,11 +167,12 @@ func (app *application) updateEventHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var input struct {
-		Title       *string `json:"title"`
-		Description *string `json:"description"`
-		Content     *string `json:"content"`
-		Lang        *string `json:"lang"`
-		Cover       *string `json:"cover"`
+		Title       *string    `json:"title"`
+		Description *string    `json:"description"`
+		Content     *string    `json:"content"`
+		Lang        *string    `json:"lang"`
+		Cover       *string    `json:"cover"`
+		OccuredOn   *time.Time `json:"occuredOn"`
 	}
 
 	err = app.readJson(w, r, &input)
@@ -195,6 +199,10 @@ func (app *application) updateEventHandler(w http.ResponseWriter, r *http.Reques
 
 	if input.Cover != nil {
 		event.Cover = *input.Cover
+	}
+
+	if input.OccuredOn != nil {
+		event.OccuredOn = *input.OccuredOn
 	}
 
 	v := validator.New()
--- internal/data/events.go
+++ internal/data/events.go
@@ -15,6 +15,7 @@ type Event struct {
 	ID          int64     `json:"id"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
+	OccuredOn   time.Time `json:"occuredOn"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Content     string    `json:"content"`
@@ -36,12 +37,13 @@ func ValidateEvent(v *validator.Validator, event *Event) {
 	v.Check(event.Lang != "", "lang", "must not be empty")
 	v.Check(validator.In(event.Lang, "ua", "en"), "lang", "must be either ua or en")
 	v.Check(event.Cover != "", "cover", "must not be empty")
+	v.Check(!event.OccuredOn.IsZero(), "occuredOn", "must be a valid date")
 }
 
 func (m EventModel) Insert(event *Event) error {
 	query := `
-		INSERT INTO events (title, description, content, lang, cover, user_id)
-		VALUES ($1, $2, $3, $4, $5, $6)
+		INSERT INTO events (title, description, content, lang, cover, occured_on, user_id)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
 		RETURNING id, version`
 
 	args := []interface{}{
@@ -50,6 +52,7 @@ func (m EventModel) Insert(event *Event) error {
 		event.Content,
 		event.Lang,
 		event.Cover,
+		event.OccuredOn,
 		event.UserID,
 	}
 
@@ -113,7 +116,7 @@ func (m EventModel) Get(id int64) (*Event, error) {
 	}
 
 	query := `
-		SELECT e.id, e.title, e.description, e.content, e.lang, e.cover, e.version
+		SELECT e.id, e.created_at, e.occured_on, e.updated_at, e.title, e.description, e.content, e.lang, e.cover, e.version
 		FROM events e
 		WHERE id = $1`
 
@@ -123,6 +126,9 @@ func (m EventModel) Get(id int64) (*Event, error) {
 	var event Event
 	err := m.DB.QueryRow(ctx, query, id).Scan(
 		&event.ID,
+		&event.CreatedAt,
+		&event.OccuredOn,
+		&event.UpdatedAt,
 		&event.Title,
 		&event.Description,
 		&event.Content,
@@ -145,8 +151,8 @@ func (m EventModel) Get(id int64) (*Event, error) {
 func (m EventModel) Update(event *Event) error {
 	query := `
 		UPDATE events
-		SET title = $1, description = $2, content = $3, lang = $4, cover = $5, updated_at = now(), version = version + 1
-		WHERE id = $6 AND version = $7
+		SET title = $1, description = $2, content = $3, lang = $4, cover = $5, occured_on = $6, updated_at = now(), version = version + 1
+		WHERE id = $7 AND version = $8
 		RETURNING version`
 
 	args := []interface{}{
@@ -155,6 +161,7 @@ func (m EventModel) Update(event *Event) error {
 		event.Content,
 		event.Lang,
 		event.Cover,
+		event.OccuredOn,
 		event.ID,
 		event.Version,
 	}
