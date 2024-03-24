0cbde6eef2ad72c5346ba1e4dfd572dcf037db35
Lex Miin <alexmiyin@gmail.com>
2024-03-24 17:00:15 +0200

    feat(admin): add Dropdown component

---
+++ apps/admin/src/lib/components/Dropdown/Dropdown.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { createDropdownMenu } from '@melt-ui/svelte'
+  import { setContext } from 'svelte'
+
+  export function show() {
+    $open = true
+  }
+
+  export function dismiss() {
+    $open = false
+  }
+
+  const ctx = createDropdownMenu({ forceVisible: true })
+  setContext('dropdown', ctx)
+
+  const {
+    states: { open }
+  } = ctx
+</script>
+
+<slot />
