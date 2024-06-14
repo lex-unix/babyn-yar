3c8fbc63fc8f87769f80be99cb61caf4ff2c1386
Lex Miin <alexmiyin@gmail.com>
2024-06-14 16:09:03 +0300

    feat: add new `media_articles` table

---
+++ migrations/000018_add_media_articles_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS media_articles (
+  id bigserial PRIMARY KEY,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  occurred_on date NOT NULL DEFAULT NOW(),
+  title text NOT NULL,
+  description text NOT NULL,
+  content text NOT NULL,
+  lang text NOT NULL,
+  cover text NOT NULL,
+  version integer NOT NULL DEFAULT 1,
+  user_id bigint NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
