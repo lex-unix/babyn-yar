22a3633f6af78046d1a909211d5ff678f850d529
Lex Miin <alexmiyin@gmail.com>
2024-07-15 14:26:01 +0300

    feat: add `development_concept` table

---
+++ migrations/000024_create_development_concepts_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS development_concepts (
+  id bigserial PRIMARY KEY,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  occured_on date NOT NULL DEFAULT NOW(),
+  title text NOT NULL,
+  description text NOT NULL,
+  content text NOT NULL,
+  lang text NOT NULL,
+  cover text NOT NULL,
+  version integer NOT NULL DEFAULT 1,
+  user_id bigint NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
