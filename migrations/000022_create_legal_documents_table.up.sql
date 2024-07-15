7bbab88ec8d667595e4d6b910403cbb84e1f0d43
Lex Miin <alexmiyin@gmail.com>
2024-07-15 13:44:33 +0300

    feat: add `legal_documents` table

---
+++ migrations/000022_create_legal_documents_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS legal_documents (
+  id bigserial PRIMARY KEY,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  occured_on date NOT NULL DEFAULT NOW(),
+  title text NOT NULL,
+  description text NOT NULL,
+  content text NOT NULL,
+  lang text NOT NULL,
+  cover text NOT NULL,
+  version integer NOT NULL DEFAULT 1,
+  user_id bigint NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
