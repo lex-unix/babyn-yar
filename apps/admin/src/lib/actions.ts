3d8382ce2944d5b1cf83fb5a334d994f4daa1343
Lex Miin <alexmiyin@gmail.com>
2025-03-11 11:20:53 +0200

    feat: add infiniteScroll action

--- apps/admin/src/lib/actions.ts
+++ apps/admin/src/lib/actions.ts
@@ -16,3 +16,50 @@ export function clickOutside(node: HTMLElement, { cb }: ClickOutsideOption) {
     }
   }
 }
+
+type InfiniteScrollParams = {
+  rootMargin?: string
+  threshold?: number
+  onIntersect?: () => void
+}
+
+export function infiniteScroll(
+  node: HTMLElement,
+  params: InfiniteScrollParams
+) {
+  let observer: IntersectionObserver
+  const setupObserver = () => {
+    if (observer) {
+      observer.disconnect()
+    }
+    observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          if (params.onIntersect) {
+            params.onIntersect()
+          }
+        }
+      },
+      {
+        root: null,
+        rootMargin: params.rootMargin || '0px 0px 200px 0px',
+        threshold: params.threshold || 0.1
+      }
+    )
+    observer.observe(node)
+  }
+
+  setupObserver()
+
+  return {
+    update() {
+      setupObserver()
+    },
+
+    destroy() {
+      if (observer) {
+        observer.disconnect()
+      }
+    }
+  }
+}
