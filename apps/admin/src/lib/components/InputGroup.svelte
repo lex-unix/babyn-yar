98cf34bd9ead87a74890648364c8745e8f62fffd
Lex Miin <alexmiyin@gmail.com>
2025-03-23 17:39:56 +0200

    feat: new ui components
    - Input
    - Label
    - InputGroup
    - Field
    - FieldError
    - Select
    - Dialog

---
+++ apps/admin/src/lib/components/InputGroup.svelte
@@ -0,0 +1,7 @@
+<span
+  class="relative isolate block has-[svg:first-child]:[&_input]:pl-10 sm:has-[svg:first-child]:[&_input]:pl-8
+  *:[svg]:pointer-events-none *:[svg]:absolute *:[svg]:top-3 *:[svg]:left-2.5 *:[svg]:z-10
+  *:[svg]:size-4 *:[svg]:text-zinc-500 sm:*:[svg]:top-2.5 sm:*:[svg]:left-2.5"
+>
+  <slot />
+</span>
---
+++ apps/admin/src/lib/components/SelectOption.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { melt, type Select } from '@melt-ui/svelte'
+  import { CheckIcon } from 'lucide-svelte'
+  import { getContext } from 'svelte'
+
+  export let value: string
+
+  const {
+    elements: { option },
+    helpers: { isSelected }
+  } = getContext<Select<string>>('select')
+
+</script>
+
+<div
+  use:melt={$option({ value })}
+  class="group/option grid cursor-default grid-cols-[--spacing(5)_1fr] items-baseline gap-x-2 rounded-lg py-2.5 pr-3.5 pl-2 text-base/6 text-zinc-950
+  data-[highlighted]:bg-blue-500 data-[highlighted]:text-white sm:grid-cols-[--spacing(4)_1fr] sm:py-1.5 sm:pr-3 sm:pl-1.5 sm:text-sm/6 data-[highlighted]:**:[svg]:text-white"
+>
+  {#if $isSelected(value)}
+    <CheckIcon class="relative size-5 self-center stroke-current sm:size-4" />
+  {/if}
+  <span
+    class="*:[svg]: col-start-2 flex min-w-0 items-center *:[svg]:size-5 *:[svg]:shrink-0 *:[svg]:text-zinc-500
+    sm:*:[svg]:size-4"
+  >
+    <slot name="icon" />
+    <span
+      class="ml-2.5 truncate first:ml-0 sm:ml-2 sm:first:ml-0"
+    >
+      <slot />
+    </span>
+  </span>
+</div>
