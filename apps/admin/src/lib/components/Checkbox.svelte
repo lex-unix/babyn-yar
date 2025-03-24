e7febc5b96f79ce56b8f54c0e11ccaf6f7204c74
Lex Miin <alexmiyin@gmail.com>
2025-03-24 17:29:15 +0200

    feat: add checkbox component

---
+++ apps/admin/src/lib/components/Checkbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  export let checked = false
+  const dispatch = createEventDispatcher()
+</script>
+
+<input
+  type="checkbox"
+  on:input={() => dispatch('input')}
+  {checked}
+  class="accent-zinc-950"
+/>
