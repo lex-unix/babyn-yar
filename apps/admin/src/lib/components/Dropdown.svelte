1b8fcfcb6b4137d53122fd50a05941c08c0323ac
Lex Miin <alexmiyin@gmail.com>
2025-03-25 16:38:40 +0200

    feat: add Dropdown component

---
+++ apps/admin/src/lib/components/Dropdown.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { createDropdownMenu, createSync } from '@melt-ui/svelte'
+  import { setContext } from 'svelte'
+
+  export let open: boolean = false
+
+  const ctx = createDropdownMenu({
+    defaultOpen: true,
+    forceVisible: true,
+    onOpenChange: ({ next }) => {
+      return next
+    }
+  })
+  setContext('dropdown', ctx)
+
+  const { states } = ctx
+
+  const sync = createSync(states)
+  $: sync.open(open, v => (open = v))
+</script>
+
+<slot />
---
+++ apps/admin/src/lib/components/DropdownMenu.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { cn } from '$lib/cn'
+  import { melt, type DropdownMenu } from '@melt-ui/svelte'
+  import { getContext } from 'svelte'
+
+  let className = ''
+  export { className as class }
+
+  const {
+    elements: { menu },
+    states: { open }
+  } = getContext<DropdownMenu>('dropdown')
+</script>
+
+{#if $open}
+  <div
+    use:melt={$menu}
+    class={cn(
+      'isolate grid grid-cols-[auto_1fr] overflow-y-auto rounded-xl bg-white/75 p-1 shadow-lg ring-1 ring-zinc-950/10 outline outline-transparent backdrop-blur-xl focus:outline-hidden',
+      className
+    )}
+  >
+    <slot />
+  </div>
+{/if}
