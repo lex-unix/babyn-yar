9d433603d9d8242ca57fed98552c89af657b1aab
Lex Miin <hello@lexunix.me>
2026-06-13 18:31:19 +0300

    nix: update nixpkgs, update go version

--- flake.nix
+++ flake.nix
@@ -1,53 +1,43 @@
 {
   description = "A Nix-flake-based development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      goVersion = 23;
-
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ self.overlays.default ];
-            };
-          }
-        );
-    in
     {
-      overlays.default = final: prev: rec {
-        go = final."go_1_${toString goVersion}";
-        nodejs = prev.nodejs;
-        yarn = (prev.yarn.override { inherit nodejs; });
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    {
+      overlays.default = final: prev: {
+        go = final.go_1_26;
+        nodejs = final.nodejs_22;
       };
-
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              go
-              nodejs
-              yarn
-              air
-              go-migrate
-              gnumake
-            ];
-          };
-        }
-      );
-    };
+    }
+    // (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.nodejs
+            pkgs.air
+            pkgs.go-migrate
+            pkgs.go-tools
+            pkgs.govulncheck
+            pkgs.just
+          ];
+        };
+      }
+    ));
 }
